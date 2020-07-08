b0465b1c06a6efe4c669d8264d1ff08f6157438c
geektutor <sodiq.akinjobi@gmail.com>
2020-07-08 14:36:59 +0100

    Fix complaint

--- staff/complain.php
+++ staff/complain.php
@@ -37,7 +37,6 @@
                 <input id="checkbox" type="checkbox"> <p class="anonymous">Send as anonymous</p>
               </div>
               <input type="hidden" name="who" value="staff">
-              <a class="dasho" href="dashboard.html">To dashboard</a>
               <textarea rows="8" cols="80" placeholder="Enter complaint..." required name="complaint"></textarea>
               <div class="submit-container">
                 <p class="paragraph">Send to: </p>
@@ -46,8 +45,6 @@
                   <?php foreach($lecturers as $name=>$mail) : ; ?>
                     <option value="<?php echo $mail; ?>"><?php echo $name; ?></option>
                   <?php endforeach; ?>
-                  <option value="themanmail@gmail.com">Course Adviser</option>
-                  <option value="3">Supervisor</option>
                   <input class="btn" type="submit" value="SUBMIT" name="submit">
                   
                 </select>
@@ -61,71 +58,7 @@
 
     </section>
 
-    <footer>
-      <div class="container">
-        <div class="go">
-          <div class="goop">
-            <img class="footer-logos" src="../img/sys.png" alt="Logo">
-            <img class="footer-logos" src="../img/lag.png" alt="Logo">
-            <div class="header-text footer">
-              <p class="footer-dept">DEPARTMENT OF <br>SYSTEMS ENGINEERING</p>
-              <h1 class="footer-uni">UNIVERSITY OF LAGOS</h1>
-            </div>
-          </div>
-
-          <div class="others">
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">History</a></li>
-              <li><a href="#">VOSO CFP</a></li>
-              <li><a href="#">Downloads</a></li>
-              <li><a href="#">News</a></li>
-              <li><a href="#">Jobs</a></li>
-            </ul>
-          </div>
-
-          <div class="others">
-            <ul>
-              <li><a href="#">Gallery</a></li>
-                <li><a href="#">Staff</a></li>
-                  <li><a href="#">Seminars</a></li>
-                  <li><a href="#">Projects</a></li>
-                  <li><a href="#">Alumni page</a></li>
-                  <li><a href="#">Archives</a></li>
-                </ul>
-            </div>
-
-            <div class="others">
-              <button class="pro">Programs <img src="../img/chevron.svg" alt=""></button>
-              <div class="saved">
-                <a class="over" href="#">Objectives</a>
-                <a class="over" href="#">Undergraduate</a>
-                <a class="over" href="#">Postgraduate</a>
-                <a class="over" href="#">Admission</a>
-              </div>
-            </div>
-
-            <div class="others">
-              <button class="pro">About ASES <img src="../img/chevron.svg" alt=""></button>
-              <div class="saved">
-                <a class="over" href="#">The Executives</a>
-                <a class="over" href="#">ASES Programmes</a>
-              </div>
-            </div>
-
-            <div class="logo-container yeba">
-              <img class="logos" src="../img/sys.png" alt="Logo">
-              <img class="logos" src="../img/lag.png" alt="Logo">
-              <div class="header-text">
-                <p class="dept">DEPARTMENT OF SYSTEMS ENGINEERING</p>
-                <h1 class="uni">UNIVERSITY OF LAGOS</h1>
-              </div>
-            </div>
-        </div>
-      </div>
-
-    </footer>
-
+    <?php include ('../config/inc_footer.php'); ?>
 
     <script src="code.js" charset="utf-8"></script>
   </body>
