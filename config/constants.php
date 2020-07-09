d9c7bcd4a080a7a9fb3e6502ffb165d879add063
Adekoya Adebayo <adekoya.adebayojubril@gmail.com>
2020-07-09 11:21:46 +0100

    Updated the constants

--- config/constants.php
+++ config/constants.php
@@ -4,10 +4,20 @@
 	$superviser_mail = 'mumu@mumu.com';
 	$course_adviser_mail = 'zubairidrisaweda@gmail.com';
 
-	$lecturers = [
-		'Mr. Dele' => 'dele1@gmail.com',
-		'Prof. Yomi' => 'yomishow@gmail.com',
-		'Dr. Geek' => 'daddysodiq@gmail.com'
-	];
+	$sql2 = "SELECT * FROM user WHERE type != 0";
+  	$result2 = mysqli_query($conn, $sql2);
+  	$count = mysqli_num_rows($result2);
+  	// $lecturers = mysqli_fetch_array($result2, MYSQLI_ASSOC);
+
+  	if ($count > 0) {
+        while($row = mysqli_fetch_assoc($result2)) {
+  			$lecturers[] = $row;
+  		}
+  	}
+	// $lecturers = [
+	// 	'Mr. Dele' => 'dele1@gmail.com',
+	// 	'Prof. Yomi' => 'yomishow@gmail.com',
+	// 	'Dr. Geek' => 'daddysodiq@gmail.com'
+	// ];
 
 ?>
