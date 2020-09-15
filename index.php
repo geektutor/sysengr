d29dd4787bd89f1f858fffd01ac82e071d1e25d4
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2020-09-15 18:16:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -206,8 +206,70 @@
 
     </section>
 
-    <?php include ('config/inc_footer.php'); ?>
 
+    <footer>
+        <div class="container">
+          <div class="go">
+            <div class="goop">
+              <img class="footer-logos" src="img/sys.png" alt="Logo">
+              <img class="footer-logos" src="img/lag.png" alt="Logo">
+              <div class="header-text footer">
+                <p class="footer-dept">DEPARTMENT OF <br>SYSTEMS ENGINEERING</p>
+                <h1 class="footer-uni">UNIVERSITY OF LAGOS</h1>
+              </div>
+            </div>
+
+            <div class="others">
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">History</a></li>
+                <li><a href="#">VOSO CFP</a></li>
+                <li><a href="#">Downloads</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Jobs</a></li>
+              </ul>
+            </div>
+
+            <div class="others">
+              <ul>
+                <li><a href="#">Gallery</a></li>
+                  <li><a href="#">Staff</a></li>
+                    <li><a href="#">Seminars</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Alumni page</a></li>
+                    <li><a href="#">Archives</a></li>
+                  </ul>
+              </div>
+
+              <div class="others">
+                <button class="pro">Programs <img src="img/chevron.svg" alt=""></button>
+                <div class="saved">
+                  <a class="over" href="#">Objectives</a>
+                  <a class="over" href="#">Undergraduate</a>
+                  <a class="over" href="#">Postgraduate</a>
+                  <a class="over" href="#">Admission</a>
+                </div>
+              </div>
+
+              <div class="others">
+                <button class="pro">About ASES <img src="img/chevron.svg" alt=""></button>
+                <div class="saved">
+                  <a class="over" href="#">The Executives</a>
+                  <a class="over" href="#">ASES Programmes</a>
+                </div>
+              </div>
+
+              <div class="logo-container yeba">
+                <img class="logos" src="img/sys.png" alt="Logo">
+                <img class="logos" src="img/lag.png" alt="Logo">
+                <div class="header-text">
+                  <p class="dept">DEPARTMENT OF SYSTEMS ENGINEERING</p>
+                  <h1 class="uni">UNIVERSITY OF LAGOS</h1>
+                </div>
+              </div>
+          </div>
+        </div>
+    </footer>
     <script src="config/code.js" charset="utf-8"></script>
   </body>
 </html>
